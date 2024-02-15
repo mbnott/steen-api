97f3e1f33d36a58daf3d98bdef1a688a3aaca6d2
david-almdp <david.almdp@eduge.ch>
2024-02-15 13:39:05 +0100

    Ajout du fichier de la creation de la base de donnée et l'insertion de donné dans la base de donnée

---
+++ SQL/20240215Table.sql
@@ -0,0 +1,339 @@
+-- MariaDB dump 10.19  Distrib 10.6.16-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: Steen
+-- ------------------------------------------------------
+-- Server version	10.6.16-MariaDB-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+DROP DATABASE if EXISTS Steen;
+CREATE DATABASE Steen;
+USE Steen;
+
+--
+-- Table structure for table `ajouter`
+--
+
+DROP TABLE IF EXISTS `ajouter`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ajouter` (
+  `idUtilisateur` int(11) NOT NULL,
+  `idJeu` int(11) NOT NULL,
+  PRIMARY KEY (`idUtilisateur`,`idJeu`),
+  KEY `fk_Utilisateur_has_Jeu_Jeu1_idx` (`idJeu`),
+  KEY `fk_Utilisateur_has_Jeu_Utilisateur1_idx` (`idUtilisateur`),
+  CONSTRAINT `fk_Utilisateur_has_Jeu_Jeu1` FOREIGN KEY (`idJeu`) REFERENCES `jeu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Utilisateur_has_Jeu_Utilisateur1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ajouter`
+--
+
+LOCK TABLES `ajouter` WRITE;
+/*!40000 ALTER TABLE `ajouter` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ajouter` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `appartenir`
+--
+
+DROP TABLE IF EXISTS `appartenir`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `appartenir` (
+  `idTag` int(11) NOT NULL,
+  `idJeu` int(11) NOT NULL,
+  PRIMARY KEY (`idTag`,`idJeu`),
+  KEY `fk_Tag_has_Jeu_Jeu1_idx` (`idJeu`),
+  KEY `fk_Tag_has_Jeu_Tag1_idx` (`idTag`),
+  CONSTRAINT `fk_Tag_has_Jeu_Jeu1` FOREIGN KEY (`idJeu`) REFERENCES `jeu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Tag_has_Jeu_Tag1` FOREIGN KEY (`idTag`) REFERENCES `tag` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `appartenir`
+--
+
+LOCK TABLES `appartenir` WRITE;
+/*!40000 ALTER TABLE `appartenir` DISABLE KEYS */;
+/*!40000 ALTER TABLE `appartenir` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `developper`
+--
+
+DROP TABLE IF EXISTS `developper`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `developper` (
+  `idJeu` int(11) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`idJeu`,`idUtilisateur`),
+  KEY `fk_Jeu_has_Utilisateur_Utilisateur1_idx` (`idUtilisateur`),
+  KEY `fk_Jeu_has_Utilisateur_Jeu1_idx` (`idJeu`),
+  CONSTRAINT `fk_Jeu_has_Utilisateur_Jeu1` FOREIGN KEY (`idJeu`) REFERENCES `jeu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Jeu_has_Utilisateur_Utilisateur1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `developper`
+--
+
+LOCK TABLES `developper` WRITE;
+/*!40000 ALTER TABLE `developper` DISABLE KEYS */;
+/*!40000 ALTER TABLE `developper` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `evaluation`
+--
+
+DROP TABLE IF EXISTS `evaluation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `evaluation` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `recommander` tinyint(2) NOT NULL,
+  `datePost` date NOT NULL,
+  `description` mediumtext NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
+  `idJeu` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_Evaluation_Utilisateur1_idx` (`idUtilisateur`),
+  KEY `fk_Evaluation_Jeu1_idx` (`idJeu`),
+  CONSTRAINT `fk_Evaluation_Jeu1` FOREIGN KEY (`idJeu`) REFERENCES `jeu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Evaluation_Utilisateur1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `evaluation`
+--
+
+LOCK TABLES `evaluation` WRITE;
+/*!40000 ALTER TABLE `evaluation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `evaluation` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `infraction`
+--
+
+DROP TABLE IF EXISTS `infraction`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `infraction` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `dateCreation` date NOT NULL,
+  `datePeremption` date DEFAULT NULL,
+  `type` varchar(30) NOT NULL,
+  `description` mediumtext NOT NULL,
+  `idAdmin` int(11) NOT NULL,
+  `idEmetteur` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_Infraction_Utilisateur1_idx` (`idAdmin`),
+  KEY `fk_Infraction_Utilisateur2_idx` (`idEmetteur`),
+  CONSTRAINT `fk_Infraction_Utilisateur1` FOREIGN KEY (`idAdmin`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Infraction_Utilisateur2` FOREIGN KEY (`idEmetteur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `infraction`
+--
+
+LOCK TABLES `infraction` WRITE;
+/*!40000 ALTER TABLE `infraction` DISABLE KEYS */;
+/*!40000 ALTER TABLE `infraction` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `jeu`
+--
+
+DROP TABLE IF EXISTS `jeu`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `jeu` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(100) NOT NULL,
+  `DateSortie` date NOT NULL,
+  `Description` mediumtext NOT NULL,
+  `prix` decimal(15,2) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `jeu`
+--
+
+LOCK TABLES `jeu` WRITE;
+/*!40000 ALTER TABLE `jeu` DISABLE KEYS */;
+/*!40000 ALTER TABLE `jeu` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `publier`
+--
+
+DROP TABLE IF EXISTS `publier`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publier` (
+  `idJeu` int(11) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`idJeu`,`idUtilisateur`),
+  KEY `fk_Jeu_has_Utilisateur_Utilisateur2_idx` (`idUtilisateur`),
+  KEY `fk_Jeu_has_Utilisateur_Jeu2_idx` (`idJeu`),
+  CONSTRAINT `fk_Jeu_has_Utilisateur_Jeu2` FOREIGN KEY (`idJeu`) REFERENCES `jeu` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Jeu_has_Utilisateur_Utilisateur2` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `publier`
+--
+
+LOCK TABLES `publier` WRITE;
+/*!40000 ALTER TABLE `publier` DISABLE KEYS */;
+/*!40000 ALTER TABLE `publier` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+INSERT INTO `role` VALUES (1,'Admin'),(2,'Utilisateur');
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tag`
+--
+
+DROP TABLE IF EXISTS `tag`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tag` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tag`
+--
+
+LOCK TABLES `tag` WRITE;
+/*!40000 ALTER TABLE `tag` DISABLE KEYS */;
+INSERT INTO `tag` VALUES (1,'fps'),(2,'sandbox'),(3,'Aventure'),(4,'openWorld'),(5,'Horreur'),(6,'strategy');
+/*!40000 ALTER TABLE `tag` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `token`
+--
+
+DROP TABLE IF EXISTS `token`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `token` (
+  `Token` varchar(200) NOT NULL,
+  `idUtilisateur` int(11) NOT NULL,
+  PRIMARY KEY (`Token`),
+  UNIQUE KEY `Token_UNIQUE` (`Token`),
+  KEY `fk_Token_Utilisateur1_idx` (`idUtilisateur`),
+  CONSTRAINT `fk_Token_Utilisateur1` FOREIGN KEY (`idUtilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `token`
+--
+
+LOCK TABLES `token` WRITE;
+/*!40000 ALTER TABLE `token` DISABLE KEYS */;
+/*!40000 ALTER TABLE `token` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `utilisateur` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `Nom` varchar(90) NOT NULL,
+  `EMail` varchar(90) NOT NULL,
+  `mdp` varchar(100) NOT NULL,
+  `muet` tinyint(1) NOT NULL,
+  `idRole` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  UNIQUE KEY `Nom_UNIQUE` (`Nom`),
+  KEY `fk_Utilisateur_Role1_idx` (`idRole`),
+  CONSTRAINT `fk_Utilisateur_Role1` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+LOCK TABLES `utilisateur` WRITE;
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES (1,'Admin','Admin@gmail.com','Super2024',0,1),(2,'premierUtilisateur','premierUtilisateur@gmail.com','Super2024',0,2);
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-15 13:34:00
