7f6105204d95a7e1d784cb81e87a0a36a852b785
KomasanGamer2 <david.almdp@eduge.ch>
2024-02-29 13:21:12 +0100

    Create donnée.sql

---
+++ sql/donnée.sql
@@ -0,0 +1,19 @@
+Use Steen;
+
+INSERT INTO tag (Nom) VALUES ("Horreur"),
+    ("Bac a sable"),
+    ("FPS"),
+    ("Metroidvania");
+
+INSERT INTO jeu (Nom, DateSortie, Description) VALUES ("La Joie De Créer", "2013-03-12", "La joie de création est un jeu d’horreur terrifiant en vue à la première personne");
+
+INSERT INTO role (Nom) VALUES ("Client"), ("Admin");
+
+INSERT INTO utilisateur (Nom, EMail, mdp, muet, idRole) VALUES ("SuperUtilisateur", "Utilisateur@gmail.com", "Super2024", 0, 1),
+    ("SuperAdmin", "Admin@gmail.com", "Super2024", 0, 2);
+
+INSERT INTO JEU_A_TAG (idTag, idJeu) VALUES (1, 1);
+
+INSERT INTO UTILISATEUR_DEVELOPPE_JEU (idJeu, idUtilisateur) VALUES (1, 1);
+
+INSERT INTO UTILISATEUR_PUBLIE_JEU (idJeu, idUtilisateur) VALUES (1, 1);
