9c97e5768ad536d8dcd5ac2bf6dc6b82d11e4e0d
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-07 13:49:02 +0100

    Update data.sql
    Updated the file data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -1,20 +1,16 @@
 USE Steen;
 
-INSERT INTO tag (Nom) VALUES ("Horreur"),
+INSERT INTO tag (nom) VALUES ("Horreur"),
     ("Bac a sable"),
     ("FPS"),
     ("Metroidvania");
 
-INSERT INTO jeu (Nom, DateSortie, Description) VALUES ("La Joie De Créer", "2013-03-12", "La joie de création est un jeu d’horreur terrifiant en vue à la première personne");
-
-INSERT INTO role (Nom) VALUES ("Client"), ("Admin");
+INSERT INTO role (nom) VALUES ("Client"), ("Admin");
 
 -- le mot de passe des deux utilisateur est Super2024
-INSERT INTO utilisateur (Nom, EMail, mdp, muet, idRole) VALUES ("SuperUtilisateur", "Utilisateur@gmail.com", "$2y$10$NZxH.5R6GaoctqV7ioiaNux/jVufSQCtE.O5brKVfuTk45i0yuA9K", 0, 1),
+INSERT INTO utilisateur (nom, email, mdp, muet, idRole) VALUES ("SuperUtilisateur", "Utilisateur@gmail.com", "$2y$10$NZxH.5R6GaoctqV7ioiaNux/jVufSQCtE.O5brKVfuTk45i0yuA9K", 0, 1),
     ("SuperAdmin", "Admin@gmail.com", "$2y$10$NZxH.5R6GaoctqV7ioiaNux/jVufSQCtE.O5brKVfuTk45i0yuA9K", 0, 2);
 
+INSERT INTO jeu (nom, DateSortie, Description, idDeveloppeur) VALUES ("La Joie De Créer", "2013-03-12", "La joie de création est un jeu d’horreur terrifiant en vue à la première personne", 1);
+
 INSERT INTO JEU_A_TAG (idTag, idJeu) VALUES (1, 1);
-
-INSERT INTO UTILISATEUR_DEVELOPPE_JEU (idJeu, idUtilisateur) VALUES (1, 1);
-
-INSERT INTO UTILISATEUR_PUBLIE_JEU (idJeu, idUtilisateur) VALUES (1, 1);
