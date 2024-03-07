b40e7a8c410e469ceaa330e683828e7a6f1d8964
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-07 15:41:27 +0100

    Update view.sql
    Updated the file view.sql

--- sql/view.sql
+++ sql/view.sql
@@ -1,7 +1,9 @@
-DROP VIEW Get_Games;
+USE Steen;
+DROP VIEW IF EXISTS Get_Games;
 CREATE VIEW `Get_Games` AS
-SELECT jeu.nom, jeu.dateSortie, jeu.description, utilisateur.nom AS pseudo, GROUP_CONCAT(tag.nom SEPARATOR ',') as tag
+SELECT jeu.nom, jeu.dateSortie, jeu.description, utilisateur.nom AS pseudo, GROUP_CONCAT(tag.nom SEPARATOR ',') as tag, AVG(evaluation.note) as note
 FROM jeu
 JOIN utilisateur ON utilisateur.id = jeu.idDeveloppeur
 JOIN JEU_A_TAG AS JAT ON JAT.idJeu = jeu.id
-JOIN tag ON tag.id = JAT.idTag;
+JOIN tag ON tag.id = JAT.idTag
+LEFT JOIN evaluation ON evaluation.idJeu = jeu.id
