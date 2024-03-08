111d7cc8fc0cfa697a131b06f6c6d59972a37be7
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-08 09:04:21 +0100

    Update data.sql

--- sql/data.sql
+++ sql/data.sql
@@ -25,7 +25,7 @@ INSERT INTO role (nom) VALUES ("Client"), ("Admin");
 INSERT INTO utilisateur (nom, email, mdp, idRole) VALUES ("SuperUtilisateur", "Utilisateur@gmail.com", "$2y$10$NZxH.5R6GaoctqV7ioiaNux/jVufSQCtE.O5brKVfuTk45i0yuA9K", 1),
     ("SuperAdmin", "Admin@gmail.com", "$2y$10$NZxH.5R6GaoctqV7ioiaNux/jVufSQCtE.O5brKVfuTk45i0yuA9K", 2);
 
-INSERT INTO jeu (nom, DateSortie, Description, idDeveloppeur) VALUES ("La Joie De Créer", "2013-03-12", "La joie de création est un jeu d’horreur terrifiant en vue à la première personne.", 1),
+INSERT INTO jeu (nom, dateSortie, description, idDeveloppeur) VALUES ("La Joie De Créer", "2013-03-12", "La joie de création est un jeu d’horreur terrifiant en vue à la première personne.", 1),
     ("La redemption de la mort rouge II", "2018-26-10", "La redemption de la mort rouge II est le deuxième opus de la série qui se déroule avant le premier jeu.", 1),
     ("La légende de Zelda : Souffle de la nature", "2017-03-03", "La légende de Zelda : Souffle de la nature est un jeu en monde ouvert dont le but est de tuer un être maléfique.");
 
