8be0b389d99e5c03c51cf9293e718c48e7ccb232
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-14 15:53:17 +0100

    Update accountController.php

--- src/controllers/accountController.php
+++ src/controllers/accountController.php
@@ -2,6 +2,7 @@
 
 namespace Mbnot\SteenApi\controllers;
 
+use Mbnot\SteenApi\models\dbManager;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Mbnot\SteenApi\Models\HTTP_STATUS;
@@ -33,7 +34,6 @@ class accountController
         if (count($missingFields) > 0)
         {
             // Keys are missing, bad request
-            http_response_code(400);
             $response->getBody()->write(json_encode([
                 "error" => "Bad Request",
                 "message" => "Fields are missing from the body of your request",
@@ -41,14 +41,33 @@ class accountController
             ]));
             return $response
                 ->withHeader('content-type', 'application/json')
-                ->withStatus(HTTP_STATUS::CREATED->value);
+                ->withStatus(HTTP_STATUS::BAD_REQUEST->value);
         }
 
         // Validating fields
-
+        if (strlen($input->email) <= 5)
+        {
+            $response->getBody()->write(json_encode([
+                "error" => "Bad Request",
+                "message" => "Email must be longer than 5 characters"
+            ]));
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(HTTP_STATUS::BAD_REQUEST->value);
+        }
 
         // Adding user to db
+        $password = password_hash($input->password, PASSWORD_BCRYPT);
 
+        $db = new dbManager();
+        $userId = $db->register($input->username, $input->email, $password);
+        $token = $db->createToken($userId);
+
+        $response->getBody()->write(json_encode([
+            "message" => "Account successfully created!",
+            "id" => $userId,
+            "token" => $token
+        ]));
         return $response
             ->withHeader('content-type', 'application/json')
             ->withStatus(HTTP_STATUS::CREATED->value);
