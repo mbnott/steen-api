2d1e5f47b73c9540071fcaa4702bc9eb0b475496
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-14 13:49:55 +0100

    Update gamesController.php
    Added some code in gameController.php

--- src/controllers/gamesController.php
+++ src/controllers/gamesController.php
@@ -3,6 +3,7 @@
 namespace Mbnot\SteenApi\controllers;
 use Mbnot\SteenApi\models\HTTP_STATUS;
 use Mbnot\SteenApi\models\dbManager;
+use Mbnot\SteenApi\models\Game;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
@@ -13,11 +14,27 @@ class gamesController
     // Fetches all games, with `tag` and `name` filter
     public function fetchAll(Request $request, Response $response): Response
     {
-        //
-        $request->getQueryParams();
+        //$request->getQueryParams();
+        $db = new dbManager();
+
+        $fetchedGames = $db->getGames();
+        $games = [];
+
+        foreach($fetchedGames as $game)
+        {
+            array_push($games, new Game(
+                $game["id"],
+                $game["nom"],
+                $game["dateSortie"],
+                $game["description"],
+                $game["pseudo"],
+                $game["tags"],
+                $game["note"],
+            ));
+        }
 
         $response->getBody()->write(json_encode(
-
+            $games
         ));
 
         return $response
@@ -27,6 +44,6 @@ class gamesController
 
     public function fetch(Request $request, Response $response, array $args) : Response
     {
-
+        
     }
 }
