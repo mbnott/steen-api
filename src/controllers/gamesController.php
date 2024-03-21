9e5e3bfd8d3b659bc7cbf83d0554f6d852a3efc8
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-21 14:48:09 +0100

    Update gamesController.php
    Created the function getGame and addGame

--- src/controllers/gamesController.php
+++ src/controllers/gamesController.php
@@ -14,7 +14,6 @@ class gamesController
     // Fetches all games, with `tag` and `name` filter
     public function getGames(Request $request, Response $response): Response
     {
-        //$request->getQueryParams();
         $db = new dbManager();
 
         $fetchedGames = $db->getGames();
@@ -41,8 +40,55 @@ class gamesController
             ->withStatus(HTTP_STATUS::OK->value);
     }
 
-    public function fetch(Request $request, Response $response, array $args) : Response
+    public function getGame(Request $request, Response $response, array $args) : Response
     {
         $db = new dbManager();
+
+        $fetchedGame = $db->getGame();
+        $game = [];
+
+        array_push($game, new Game(
+            $game["id"],
+            $game["nom"],
+            $game["dateSortie"],
+            $game["description"],
+            $game["pseudo"],
+            $game["note"],
+        ));
+
+        $response->getBody()->write(json_encode(
+            $game
+        ));
+
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(HTTP_STATUS::OK->value);
+    }
+
+    public function addGame(Request $request, Response $response): Response
+    {
+        // Getting body data
+        $input = json_decode(file_get_contents('php://input'));
+
+        // Checking if all fields are present
+        $fields = ["nom", "dateSortie", "description", "idDeveloppeur"];
+        $missingFields = array_values(array_diff($fields, array_keys((array)$input)));
+        if (count($missingFields) > 0) {
+            return $response
+                ->withHeader('content-type', 'application/json')
+                ->withStatus(HTTP_STATUS::BAD_REQUEST->value);
+        }
+
+        // Adding game to db
+        $db = new dbManager();
+        $gameId = $db->addGame($input->nom, $input->dateSortie, $input->description, $input->idDeveloppeur);
+
+        $response->getBody()->write(json_encode([
+            "message" => "Game successfully added!",
+            "id" => $gameId,
+        ]));
+        return $response
+            ->withHeader('content-type', 'application/json')
+            ->withStatus(HTTP_STATUS::CREATED->value);
     }
 }
