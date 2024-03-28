ca9b95e5eb7bb914d2dd626ba29b4ac8f695d0e7
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-28 15:37:51 +0100

    Update view.sql

--- sql/view.sql
+++ sql/view.sql
@@ -13,7 +13,7 @@ GROUP BY jeu.id;
 
 DROP VIEW IF EXISTS getGames;
 CREATE VIEW `getGames` AS
-SELECT jeu.id,  jeu.nom, jeu.dateSortie, jeu.description, utilisateur.nom AS pseudo, AVG(evaluation.note) as note
+SELECT jeu.id,  jeu.nom, jeu.dateSortie, jeu.description, jeu.idDeveloppeur, utilisateur.nom AS pseudo, AVG(evaluation.note) as note
 FROM jeu
 JOIN utilisateur ON utilisateur.id = jeu.idDeveloppeur
 LEFT JOIN evaluation ON evaluation.idJeu = jeu.id
