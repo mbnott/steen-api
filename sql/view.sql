e5ce56ac731214722f2386b98da49c0e0f546bd4
KomasanGamer2 <david.almdp@eduge.ch>
2024-03-28 15:06:03 +0100

    Update view.sql

--- sql/view.sql
+++ sql/view.sql
@@ -1,4 +1,6 @@
+SET sql_mode = 'TRADITIONAL';
 USE Steen;
+
 DROP VIEW IF EXISTS getGamesWTag;
 CREATE VIEW `getGamesWTag` AS
 SELECT jeu.id,  jeu.nom, jeu.dateSortie, jeu.description, utilisateur.nom AS pseudo, GROUP_CONCAT(tag.nom SEPARATOR ',') as tags, AVG(evaluation.note) as note
@@ -16,3 +18,10 @@ FROM jeu
 JOIN utilisateur ON utilisateur.id = jeu.idDeveloppeur
 LEFT JOIN evaluation ON evaluation.idJeu = jeu.id
 GROUP BY jeu.id;
+
+DROP VIEW IF EXISTS getReviews;
+CREATE VIEW `getReviews` AS
+SELECT evaluation.id,  evaluation.note, evaluation.datePost, evaluation.description, utilisateur.nom AS pseudo, jeu.nom as jeu
+FROM evaluation
+JOIN utilisateur ON utilisateur.id = evaluation.idUtilisateur
+JOIN jeu ON jeu.id = evaluation.idJeu;
