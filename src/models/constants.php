55adbc56b6f34351093298fc2f0ffaf2c630f8a6
mbnot <mbnot@pm.me>
2024-09-19 21:48:03 +0200

    fix: :pencil2: Change PHP db constants to match db.sql

--- src/models/constants.php
+++ src/models/constants.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOST = 'localhost';
 const DB_NAME = 'Steen';
-const DB_USER = '***REMOVED***';
-const DB_PASSWORD = '***REMOVED***';
+const DB_USER = 'SteenUser';
+const DB_PASSWORD = 'admin';
 const DB_CHARSET = 'utf8mb4';
